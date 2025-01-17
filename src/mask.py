261e2e39148700949c090029d017568f5e042d10
Игорь Якимов <natsu.dragonnil@gmail.com>
2025-01-17 23:55:00 +1200

    Add tests to module "mask" function "get_mask_account"

--- src/mask.py
+++ src/mask.py
@@ -13,6 +13,6 @@ def get_mask_account(account: Union[int, str]) -> str:
     return f"**{str(account)[-4:]}"
 
 
-if __name__ == "__main__":
-    print(get_mask_card_number(7000792289606361))
-    print(get_mask_account(73654108430135874305))
+# if __name__ == "__main__":
+#     print(get_mask_card_number(7000792289606361))
+#     print(get_mask_account(73654108430135874305))
